5f090278e8c254cb0a676a016f7e68c4ca60bea2
kyukyunyorituryo <kyukyunyorituryo@live.jp>
2020-07-23 23:14:39 +0900

    カレンダー修正

--- js/calendar.js
+++ js/calendar.js
@@ -1,7 +1,7 @@
             $(function () {
                 $('#datetimepicker1').datetimepicker({
                minDate:'2020/01/01',
-               maxDate:'2020/09/31',
+               maxDate:'2020/09/30',
 //               disabledDates:['2020/07/26'],
             dayViewHeaderFormat: 'YYYY年 MMMM',
             tooltips: {
