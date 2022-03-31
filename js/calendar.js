270bffd61b6a736f066f9c2045fc930ca76c92d2
kyukyunyorituryo <kyukyunyorituryo@live.jp>
2022-04-01 01:40:23 +0900

    カレンダーの修正

--- js/calendar.js
+++ js/calendar.js
@@ -1,7 +1,7 @@
             $(function () {
                 $('#datetimepicker1').datetimepicker({
                minDate:'2020/01/01',
-               maxDate:'2022/03/31',
+               maxDate:'2022/04/30',
 //               disabledDates:['2020/07/26'],
             dayViewHeaderFormat: 'YYYY年 MMMM',
             tooltips: {
